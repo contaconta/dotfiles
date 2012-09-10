ad1e027f3beff1aec2fc2699c39f59808bc32d11
Takanori Ogata <conta1031@gmail.com>
2012-09-10 11:46:13 +0900

    change init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -33,14 +33,15 @@
 (define-key global-map (kbd "C-h") 'delete-backward-char) ; 削除
 (define-key global-map (kbd "M-?") 'help-for-help)        ; ヘルプ
 (define-key global-map (kbd "C-z") 'undo)                 ; undo
-(define-key global-map (kbd "C-/") 'undo)                 ; undo
+(define-key global-map (kbd "C-x /") 'undo)                 ; undo
 (define-key global-map (kbd "C-c i") 'indent-region)      ; インデント
 ;(define-key global-map (kbd "C-c C-i") 'hippie-expand)    ; 補完
 (define-key global-map (kbd "C-c ;") 'comment-dwim)       ; コメントアウト
+(define-key global-map (kbd "C-c :") 'uncomment-region)    ; コメント解除
 ;(define-key global-map (kbd "C-o") 'toggle-input-method)  ; 日本語入力切替
 (define-key global-map (kbd "M-C-g") 'grep)               ; grep
 (define-key global-map (kbd "C-m") 'newline-and-indent)
-
+(define-key global-map (kbd "C-q") 'suspend-emacs) ; suspend
 
 ;; Stop Auto Backup
 (setq make-backup-files nil)
