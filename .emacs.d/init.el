9eb8870eeda3629badcdaa4ca86feea8748e0fd7
Takanori Ogata <conta1031@gmail.com>
2014-03-06 01:31:41 +0900

    add cc-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -255,3 +255,25 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;(el-get 'sync '(powerline-1.2))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; cc-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq c-default-style "linux"
+      c-basic-offset 4
+      tab-width 4)
+
+;; (add-to-list 'load-path "~/.emacs.d/lisp/emacs-clang-complete-async") 
+;; (require 'auto-complete-clang-async)
+
+;; (defun ac-cc-mode-setup ()
+;;     (setq ac-clang-complete-executable "~/.emacs.d/clang-complete")
+;;       (setq ac-sources '(ac-source-clang-async))
+;;         (ac-clang-launch-completion-process)
+;;         )
+
+;; (defun my-ac-config ()
+;;     (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+;;       (add-hook 'auto-complete-mode-hook 'ac-common-setup))
+
+;; (my-ac-config)                          
