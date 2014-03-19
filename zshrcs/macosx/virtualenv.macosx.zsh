d56148df5b9bae9c09c3ce0585ba141ed412cc82
Takanori Ogata <conta1031@gmail.com>
2014-03-19 16:19:03 +0900

    create function

--- zshrcs/macosx/virtualenv.macosx.zsh
+++ zshrcs/macosx/virtualenv.macosx.zsh
@@ -5,10 +5,12 @@ echo "mac virtualenv..."
 #   so if you don't want to display "(env01) $" to prompt,
 #   run this script before "source $ZSH/oh-my-zsh.sh"
 ###############################################################################
-if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
-   export WORKON_HOME=$HOME/.virtualenvs
-   export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
-   source /usr/local/bin/virtualenvwrapper.sh
-   export VIRTUALENV_USE_DISTRIBUTE=1
-   workon pydev2.7
-fi
+function set_virtualenv () {
+    if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+        export WORKON_HOME=$HOME/.virtualenvs
+        export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+        source /usr/local/bin/virtualenvwrapper.sh
+        export VIRTUALENV_USE_DISTRIBUTE=1
+        workon pydev2.7
+    fi    
+}
