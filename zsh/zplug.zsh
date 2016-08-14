66281e4a1fb7d4886c329cbf72d715516799fabf
Takanori Ogata <takanori@abeja.asia>
2016-08-15 02:22:39 +0900

    remove enhancd because of the error "-q: no such option"

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -7,21 +7,16 @@ fi
 # Essential
 source ~/.zplug/init.zsh
 
-# zplug check returns true if all packages are installed
-# Therefore, when it returns false, run zplug install
-if ! zplug check; then
-    zplug install
-fi
-
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-syntax-highlighting", nice:10
 
-# https://github.com/b4b4r07/enhancd
-zplug "b4b4r07/enhancd", use:init.sh
-
 # https://github.com/zplug/zplug/wiki/Configurations
 zplug "peco/peco", as:command, from:gh-r
+
+# # https://github.com/b4b4r07/enhancd
+# ENHANCD_FILTER=peco; export ENHANCD_FILTER
+# zplug "b4b4r07/enhancd", use:init.sh
 
 # from oh-my-zsh
 zplug "lib/key-bindings", from:oh-my-zsh
@@ -30,11 +25,7 @@ zplug "lib/completion", from:oh-my-zsh
 zplug "themes/cloud", from:oh-my-zsh
 
 # Install plugins if there are plugins that have not been installed
-if ! zplug check --verbose; then
-    # printf "Install? [y/N]: "
-    # if read -q; then
-    #     echo; zplug install
-    # fi
+if ! zplug check; then
     zplug install
 fi
 
